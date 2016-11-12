036b1a100997eb9064c0cddebaca3489e28948eb
Yuriy Taraday <yorik.sar@gmail.com>
2016-11-12 23:23:33 +0300

    Add support for killing deployment pods
    Change-Id: Ifeba38fb693bb1c9578d1ab5783792c614cd1ab9

--- fuel_ccp_entrypoint/start_script.py
+++ fuel_ccp_entrypoint/start_script.py
@@ -407,9 +407,9 @@ def wait_for_deployment(obj):
     while True:
         desired = obj.obj['spec']['replicas']
         status = obj.obj['status']
-        updated = status['updatedReplicas']
-        available = status['availableReplicas']
-        current = status['replicas']
+        updated = status.get('updatedReplicas', 0)
+        available = status.get('availableReplicas', 0)
+        current = status.get('replicas', 0)
         if desired == updated == available == current:
             break
         LOG.info("Waiting for deployment %s: desired=%s, updated=%s,"
@@ -496,12 +496,15 @@ def do_provision(role_name):
     job = workflow.get("job")
     daemon = workflow.get("daemon")
     roll = workflow.get("roll")
+    kill = workflow.get("kill")
     if job:
         execute_job(workflow, job)
     elif daemon:
         execute_daemon(workflow, daemon)
     elif roll is not None:
         execute_roll(workflow, roll)
+    elif kill is not None:
+        execute_kill(workflow, kill)
     else:
         LOG.error("Job or daemon is not specified in workflow")
         sys.exit(1)
@@ -553,6 +556,27 @@ def execute_roll(workflow, roll):
     set_status_ready(workflow["name"])
     sys.exit(0)
 
+
+def execute_kill(workflow, kill):
+    LOG.info("Killing deployments for service %s", workflow["name"])
+    with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace") as f:
+        namespace = f.read()
+    client = get_pykube_client()
+    objs = []
+    for object_dict in kill:
+        if object_dict['kind'] != 'Deployment':
+            LOG.warn("Don't know how to handle %s, skipping",
+                     object_dict['kind'])
+            continue
+        obj = get_pykube_object(object_dict, namespace, client)
+        obj.reload()
+        obj.obj['spec']['replicas'] = 0
+        obj.update()
+        objs.append(obj)
+    for obj in objs:
+        wait_for_deployment(obj)
+    set_status_ready(workflow["name"])
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
